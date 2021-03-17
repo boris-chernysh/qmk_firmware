52003f6f87369c62cb5f7a97f25a1f2a4af6d7f2
Boris Chernysh <borischernysh31@gmail.com>
2021-03-17 10:12:05 +0300

    Reduced key count and optimize layers

--- keyboards/unkeeb/keymaps/default/keymap.c
+++ keyboards/unkeeb/keymaps/default/keymap.c
@@ -8,50 +8,41 @@
 #define NAV 2
 #define LMOD 3
 #define RMOD 4
-#define MOUSE 5
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = layout_two_halfs (
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-            XXXXXXX, KC_GRAVE, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET,
-            XXXXXXX, XXXXXXX, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_BSLASH,
-            XXXXXXX, XXXXXXX, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_BSLASH, XXXXXXX,
-            KC_BSPACE, LT(FN, KC_ESC), LT(LMOD, KC_SPACE), LT(MOUSE, KC_TAB), KC_ENTER, KC_ENTER, LT(MOUSE, KC_TAB), LT(RMOD, KC_SPACE), LT(NAV, KC_ESC), KC_BSPACE
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, KC_GRAVE, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET,
+            XXXXXXX, XXXXXXX, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, XXXXXXX,
+            XXXXXXX, XXXXXXX, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, XXXXXXX, XXXXXXX,
+            LT(LMOD, KC_SPACE), LT(FN, KC_BSPACE), LT(NAV, KC_BSPACE), LT(RMOD, KC_SPACE)
             ),
     [FN] = layout_two_halfs (
-            _______, KC_FN1, KC_FN2, KC_FN3, KC_FN4, KC_FN5, KC_FN6, KC_FN7, KC_FN8, KC_FN9, KC_FN10, KC_FN11, KC_FN12, _______,
-            _______, _______, _______, _______, _______, KC_AUDIO_VOL_UP, _______,  _______, _______, _______, _______, _______, _______, _______,
-            RESET, _______, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, KC_AUDIO_MUTE, _______, KC_PSCREEN, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, KC_AUDIO_VOL_DOWN, _______, _______, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+            XXXXXXX, KC_FN1, KC_FN2, KC_FN3, KC_FN4, KC_FN5, KC_FN6, KC_FN7, KC_FN8, KC_FN9, KC_FN10, KC_FN11, KC_FN12, XXXXXXX,
+            XXXXXXX, XXXXXXX, KC_AUDIO_MUTE, XXXXXXX, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            RESET, XXXXXXX, XXXXXXX, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, XXXXXXX, KC_PSCREEN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_BTN3, KC_MS_BTN2, KC_MS_BTN1, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
             ),
     [NAV] = layout_right_half (
-            _______, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______,
-            KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX,
+            KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX
             ),
     [RMOD] = layout_right_half (
             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
             KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL,
-            XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX, XXXXXXX,
-            XXXXXXX, XXXXXXX, KC_CAPS, KC_DEL, XXXXXXX, XXXXXXX, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+            XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_BSLASH, XXXXXXX,
+            KC_ENTER, KC_ESC, KC_CAPS, KC_DEL, KC_TAB, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX
             ),
     [LMOD] = layout_left_half (
             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
             XXXXXXX, XXXXXXX, KC_1, KC_2, KC_3, KC_4, KC_5,
             XXXXXXX, XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL, KC_CAPS, XXXXXXX, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-            ),
-    [MOUSE] = layout_two_halfs (
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, KC_MS_BTN3, KC_MS_BTN3, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, KC_MS_BTN2, KC_MS_BTN2, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, KC_MS_BTN1, KC_MS_BTN1, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+            XXXXXXX, XXXXXXX, KC_TAB, KC_DEL, KC_CAPS, KC_ESC, KC_ENTER,
+            XXXXXXX, XXXXXXX
             )
 };
 
@@ -98,7 +89,7 @@ void pointing_device_task(void) {
         int8_t leftxMove = (int8_t)(xperc * 127.0);
         int8_t leftyMove = (int8_t)(yperc * 127.0);
 
-        if (layer_state_is(MOUSE)) {
+        if (layer_state_is(RMOD)) {
             float scrollSpeed = 0.1 * get_speed(leftxMove, leftyMove);
             report.h = leftXPolarity * leftxMove * scrollSpeed;
             report.v = leftYPolarity * leftyMove * scrollSpeed;
@@ -120,7 +111,7 @@ void pointing_device_task(void) {
         int8_t rightxMove = (int8_t)(xperc * 127.0);
         int8_t rightyMove = (int8_t)(yperc * 127.0);
 
-        if (layer_state_is(MOUSE)) {
+        if (layer_state_is(LMOD)) {
             float cursorSpeed = 0.1 * get_speed(rightxMove, rightyMove);
             report.h = rightXPolarity * rightxMove * cursorSpeed;
             report.v = rightYPolarity * rightyMove * cursorSpeed;
